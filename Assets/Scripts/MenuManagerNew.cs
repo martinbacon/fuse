1d14d57fb842f01f946c4920c80ae6c458153de5
Martin Slanina <martin.slanina1995@gmail.com>
2016-03-19 16:35:47 +0100

    Added inventory animation and first UI

---
+++ Assets/Scripts/MenuManagerNew.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManagerNew : MonoBehaviour {
+
+    Animator anim;
+    AudioSource[] sources;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = Camera.main.GetComponent<Animator>();
+        sources = GetComponents<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void InventoryClick()
+    {
+        anim.SetBool("Inventory", true);
+        sources[1].Play();
+
+    }
+}
